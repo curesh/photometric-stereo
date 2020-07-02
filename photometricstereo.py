85ac4edc0b9a01c3f3fffd1cb74d756f66f604f0
Chandra Suresh <chandra.b.suresh@gmail.com>
2020-07-02 15:06:35 -0600

    Make changes to main script and add .gitignore file

--- photometricstereo.py
+++ photometricstereo.py
@@ -4,9 +4,13 @@ from os import listdir
 from os.path import isfile, join
 
 dir_chrome = ""
-chrome_imgs = [f for f in listdir(dir_chrome) if isfile(join(dir_chrome, f))]
+chrome_img_files = [f for f in listdir(dir_chrome) if isfile(join(dir_chrome, f))]
+for file in chorme_img_files:
+    chrome_img = cv.imread(file, 0) # Reads image in grayscale
 
 #Note that coord (relative to the image) needs to be centered at 0,0 for this to work
+def find_chrome_ref(chrome_img):
+    
 
 def find_sphere_normal(chrome_img, radius, coord):
     # Frame of reference: Assume that the center of the sphere is the origin and 
@@ -16,4 +20,5 @@ def find_sphere_normal(chrome_img, radius, coord):
     # We have information about the x and y axes, and we need to find the z-coord
     # for a complete normal vector
     norm = (coord[0], coord[1], np.sqrt(coord[0]**2 + coord[1]**2))
+    
     return norm
